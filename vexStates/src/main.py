ab04e2640fc762314388c71e0593cc91f62183a9
junnaaa <paxtonbennison@outlook.com>
2024-09-30 11:35:19 +0800

    first team program commit

---
+++ vexStates/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       noahc                                                        #
+# 	Created:      9/30/2024, 11:31:51 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
