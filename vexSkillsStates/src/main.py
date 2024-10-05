c068603c2d792b9966f3592ca1278e7a6fec987f
junnaaa <paxtonbennison@outlook.com>
2024-10-05 14:57:56 +0800

    adding the vexskills program with base driving

---
+++ vexSkillsStates/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       junnaaa                                                      #
+# 	Created:      10/5/2024, 2:51:18 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
